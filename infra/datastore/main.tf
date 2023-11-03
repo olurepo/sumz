bc40c3211d3cc63a675a4b6eaa73925e0a4976ff
Olu <olu@pearloft.com>
2023-11-03 07:05:46 -0400

    added large files to ignore

---
+++ infra/datastore/main.tf
@@ -0,0 +1,28 @@
+# import {
+#   to = aws_s3_bucket.sumz_bucket
+#   id = "sumz-bucket"
+# }
+
+
+resource "aws_s3_bucket" "sumz_bucket" {
+  bucket = "sumz-bucket"
+
+  tags = {
+    Name = "sumz-bucket"
+    env = "dev"
+  }
+}
+
+# resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
+#   bucket = aws_s3_bucket.sumz_bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "bucket_acl" {
+#   depends_on = [aws_s3_bucket_ownership_controls.bucket_controls]
+
+#   bucket = aws_s3_bucket.sumz_bucket.id
+#   acl    = "private"
+# }
