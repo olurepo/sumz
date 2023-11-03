c09f7132fa741b8bc759f1dc419daa19fc7dece1
Olu <olu@pearloft.com>
2023-11-03 19:14:22 -0400

    infra change2

--- infra/datastore/main.tf
+++ infra/datastore/main.tf
@@ -13,12 +13,12 @@ resource "aws_s3_bucket" "sumz_bucket" {
   }
 }
 
-# resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
-#   bucket = aws_s3_bucket.sumz_bucket.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
+  bucket = aws_s3_bucket.sumz_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
 # resource "aws_s3_bucket_acl" "bucket_acl" {
 #   depends_on = [aws_s3_bucket_ownership_controls.bucket_controls]
